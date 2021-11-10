bb2c35bd717c4aa9ac42ab864a870a2e22dd5e3e
bo198214 <github@eretrandrer.e4ward.com>
2021-11-10 20:02:21 +0100

    added the fast downward plans

--- solve-lilotane.sh
+++ solve-lilotane.sh
@@ -4,11 +4,12 @@ name=$1
 shift
 
 PLANNER_DIR=$HOME/workspace/planner/lilotane
-
-plan_file=$(pwd)/${name}_downward.txt
-domain_file=$(pwd)/${name}-domain.pddl
-problem_file=$(pwd)/${name}-problem.pddl
+mkdir solve-lilotane
+plan_file=$(pwd)/solve-lilotane/${name}.txt
+log_file=$(pwd)/solve-lilotane/${name}.log
+domain_file=$(pwd)/${name}-domain.hddl
+problem_file=$(pwd)/${name}-problem.hddl
 cd "$PLANNER_DIR"
-./lilotane $domain_file $problem_file "$@"
+./lilotane $domain_file $problem_file "$@" | tee $log_file
 #./run_and_verify.sh $domain_file $problem_file
 #./pandaPIparser $domain_file $problem_file
