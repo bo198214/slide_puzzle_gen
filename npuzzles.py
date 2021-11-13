b35f11b7c3d857be795844d27ca62afb64ddca53
bo198214 <github@eretrandrer.e4ward.com>
2021-11-13 15:09:55 +0100

    United npuzzles

---
+++ npuzzles.py
@@ -0,0 +1,58 @@
+import slide_puzzle_gen
+
+
+def gen(name,init_state,target_state):
+    domain, problem = slide_puzzle_gen.domain_problem(name, name, init_state, target_state)
+    print(domain, file=open("npuzzle/" + name + '-domain.pddl', 'w'))
+    print(problem, file=open("npuzzle/" + name + '-problem.pddl', 'w'))
+
+
+init_state = [
+    [ None, 8, 7],
+    [ 6, 5, 4],
+    [ 3, 2, 1]
+]
+
+target_state = {
+    (1,3): 1, (2,3): 2, (3,3): 3,
+    (1,2): 4, (2,2): 5, (3,2): 6,
+    (1,1): 7, (2,1): 8, (3,1): None
+}
+
+gen("n8puzzle01",init_state,target_state)
+
+#Easy to solve
+
+init_state = [
+    ["t15", "t02", "t01", "t12"],
+    ["t08", "t05", "t06", "t11"],
+    ["t04", "t09", "t10", "t07"],
+    ["t03", "t14", "t13", None]
+]
+
+target_state = {
+    (1,4): None,  (2,4): "t01", (3,4): "t02", (4,4): "t03",
+    (1,3): "t04", (2,3): "t05", (3,3): "t06", (4,3): "t07",
+    (1,2): "t08", (2,2): "t09", (3,2): "t10", (4,2): "t11",
+    (1,1): "t12", (2,1): "t13", (3,1): "t14", (4,1): "t15"
+}
+
+gen("n15puzzle01",init_state,target_state)
+
+# A bit more difficult to solve
+
+init_state = [
+    ["t15", "t14", "t13", "t12"],
+    ["t11", "t10", "t09", "t08"],
+    ["t07", "t06", "t05", None],
+    ["t03", "t02", "t01", "t04"]
+]
+
+target_state = {
+    (1,4): "t01", (2,4): "t02", (3,4): "t03", (4,4): "t04",
+    (1,3): "t05", (2,3): "t06", (3,3): "t07", (4,3): "t08",
+    (1,2): "t09", (2,2): "t10", (3,2): "t11", (4,2): "t12",
+    (1,1): "t13", (2,1): "t14", (3,1): "t15", (4,1): None
+}
+
+gen("n15puzzle02",init_state,target_state)
---
+++ test.py
@@ -0,0 +1,63 @@
+import slide_puzzle_gen
+
+
+def write_files(name,domain,problem):
+    print(domain, file=open("test/" + name + '-domain.pddl', 'w'))
+    print(problem, file=open("test/" + name + '-problem.pddl', 'w'))
+
+
+def battery(title,init_state,target_state):
+    name = title
+    domain, problem = slide_puzzle_gen.domain_problem(name, name, init_state, target_state)
+    write_files(name, domain, problem)
+
+    name = title + "-ac"
+    domain,problem=slide_puzzle_gen.domain_problem(name,name,init_state,target_state,
+                                                   adapted_counter=True)
+    write_files(name,domain,problem)
+
+    name = title + "-ac-it"
+    domain,problem=slide_puzzle_gen.domain_problem(name,name,init_state,target_state,
+                                                   adapted_counter=True,initial_tile="tsq")
+    write_files(name,domain,problem)
+
+    name = title + "-ac-typing"
+    domain,problem=slide_puzzle_gen.domain_problem(name,name,init_state,target_state,
+                                                   adapted_counter=True,typing=True)
+    write_files(name,domain,problem)
+
+    name = title + "-ac-it-typing"
+    domain,problem=slide_puzzle_gen.domain_problem(name,name,init_state,target_state,
+                                                   adapted_counter=True,initial_tile="tsq",typing=True)
+    write_files(name,domain,problem)
+
+    name = title + "-typing"
+    domain,problem=slide_puzzle_gen.domain_problem(name,name,init_state,target_state,
+                                                   typing=True)
+    write_files(name,domain,problem)
+
+
+init_state = [
+    [ None, 8, 7],
+    [ 6, 5, 4],
+    [ 3, 2, 1]
+]
+
+target_state = {
+    (1,3): 1, (2,3): 2, (3,3): 3,
+    (1,2): 4, (2,2): 5, (3,2): 6,
+    (1,1): 7, (2,1): 8, (3,1): None
+}
+
+battery("n8puzzle",init_state,target_state)
+
+init_state = [
+    ["tv1",  None, "tv2", "tv3"],
+    ["tv1",  None, "tv2", "tv3"],
+    ["tv4", "tsq", "tsq", "tv5"],
+    ["tv4", "tsq", "tsq", "tv5"],
+    ["to1", "to2", "to3", "to4"]
+]
+target_state = {(2, 2): "tsq", (3, 2): "tsq", (2, 1): "tsq", (3, 1): "tsq"}
+
+battery("khunpan07",init_state,target_state)
