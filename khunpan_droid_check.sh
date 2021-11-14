8f873d0c94bde0035e003e15dc17764f8adfc3f9
bo198214 <github@eretrandrer.e4ward.com>
2021-11-14 13:32:06 +0100

    updated check scripts

--- khunpan_droid_check.sh
+++ khunpan_droid_check.sh
@@ -5,11 +5,24 @@ do
   n=${problem%-problem.pddl}
   domain=${n}-domain.pddl
   plan=solve-downward/$n.txt
-  if [ -f $plan ]
+
+  echo Parser $domain $problem
+  Parser $domain $problem | grep 'Error\|Warning'
+
+  found_plan=
+  for solver in solve-*
+  do
+    if ! [ -d "$solver" ]; then continue; fi
+    plan=$solver/$n.txt
+    if [ -f $plan ]
+    then
+      echo Validate $domain $problem $plan
+      Validate $domain $problem $plan
+	  found_plan=1
+	fi
+  done
+  if [ -z "$found_plan" ]
   then
-	echo Validate $domain $problem $plan
-	Validate $domain $problem $plan
-  else
 	echo Validate $domain $problem
 	Validate $domain $problem
   fi  
--- khunpan_exercises_check.sh
+++ khunpan_exercises_check.sh
@@ -3,13 +3,27 @@
 for problem in khunpan/khunpan-exercise[0-9][0-9][0-9]-problem.pddl
 do
   n=${problem%-problem.pddl}
+ 
   domain=${n}-domain.pddl
   plan=solve-downward/$n.txt
-  if [ -f $plan ]
+
+  echo Parser $domain $problem
+  Parser $domain $problem | grep 'Error\|Warning'
+
+  found_plan=
+  for solver in solve-*
+  do
+    if ! [ -d "$solver" ]; then continue; fi
+    plan=$solver/$n.txt
+    if [ -f $plan ]
+    then
+      echo Validate $domain $problem $plan
+      Validate $domain $problem $plan
+	  found_plan=1
+	fi
+  done
+  if [ -z "$found_plan" ]
   then
-	echo Validate $domain $problem $plan
-	Validate $domain $problem $plan
-  else
 	echo Validate $domain $problem
 	Validate $domain $problem
   fi  
